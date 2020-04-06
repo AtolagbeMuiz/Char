f00dd98009a122ab7215a211133cc11f4dab76e4
AtolagbeMuiz <atolagbemuiz@gmail.com>
2020-04-06 12:45:25 -0700

    Importing Project

---
+++ char/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            char ch;
+
+            Console.WriteLine("Number Value");
+
+            for (i = 63; i <= 94; i++)
+            {
+                ch = (char) i ;
+                Console.WriteLine("{0} is {1}", i, ch);
+            }
+
+        }
+    }
+}
